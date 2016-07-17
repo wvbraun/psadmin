448f6a7954d0593cd72c5962339ef305325a4933
pnd <bp@PND.local>
2016-07-17 01:38:28 -0500

    added an authors page, lol

---
+++ src/api/authorData.js
@@ -0,0 +1,13 @@
+module.exports = {
+  authors: [
+    { id: "pnd",
+      firstName: "party",
+      lastName: "door"
+    },
+    {
+      id: "pancho",
+      firstName: "pancho",
+      lastName: "ohcnap"
+    }
+  ]
+};
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ var React = require("react");
 var Home = require("./components/homePage");
 var About = require("./components/about/aboutPage");
 var Header = require("./components/common/header");
+var Authors = require("./components/authors/authorPage");
 
 
 // one way to route (best way is w/ react-router)
@@ -15,6 +16,9 @@ var Header = require("./components/common/header");
       switch (this.props.route) {
         case "about":
           Child = About;
+          break;
+        case "authors":
+          Child = Authors;
           break;
         default:
           Child = Home;
