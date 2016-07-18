0ad39695b9e89ef33e0e6b332df0b9545a0b4863
pnd <bp@PND.local>
2016-07-17 21:00:13 -0500

    got a flux dispatcher up. need to finish tomorrow

---
+++ src/actions/authorActions.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var Dispatcher = require("./dispatcher/appDispatcher");
+var AuthorApi = require("./api/authorApi");
+var ActionTypes = require("./constants/actionTypes");
+
+
+var authorActions = {
+  createAuthor: function(author) {
+    var newAuthor = AuthorApi.saveAuthor(author);
+
+    Dispatcher.dispatch({
+      actionType: ActionTypes.CREATE_AUTHOR,
+      author: newAuthor
+    })
+
+  }
+};
+
+module.exports = Dispatcher;
